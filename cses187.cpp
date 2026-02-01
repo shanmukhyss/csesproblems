ebc38a0c30863e8692f5a22419b10b40adb4a81d
shanmukhyss <yssreenivas2006@gmail.com>
2026-02-01 10:59:01 +0530

    new changes updated

---
+++ cses187.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <cfenv>
+using namespace std;
+
+int n, a, b;
+vector<vector<long double>> memo;
+vector<vector<bool>> vis;
+
+long double dfs(int i, int s) {
+    if (s < 0) return 0.0L;
+    if (i == 0) return (s == 0 ? 1.0L : 0.0L);
+    if (vis[i][s]) return memo[i][s];
+
+    vis[i][s] = true;
+    long double res = 0.0L;
+
+    for (int face = 1; face <= 6; ++face) {
+        res += dfs(i - 1, s - face) / 6.0L;
+    }
+
+    return memo[i][s] = res;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> a >> b;
+
+    int maxSum = 6 * n;
+    memo.assign(n + 1, vector<long double>(maxSum + 1, 0.0L));
+    vis.assign(n + 1, vector<bool>(maxSum + 1, false));
+
+    long double ans = 0.0L;
+    for (int s = a; s <= b; ++s) {
+        ans += dfs(n, s);
+    }
+
+    // Round half to even
+    fesetround(FE_TONEAREST);
+    long double scaled = ans * 1e6L;
+    long double rounded = nearbyint(scaled);
+
+    cout << fixed << setprecision(6) << (rounded / 1e6L) << "\n";
+    return 0;
+}
