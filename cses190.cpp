b00b0cefed8e7cd7c76f4d9c6485a043f9b00d37
shanmukhyss <yssreenivas2006@gmail.com>
2026-02-02 12:17:17 +0530

    new changes updated

--- cses190.cpp
+++ cses190.cpp
@@ -1,46 +1,19 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <iomanip>
 using namespace std;
-long double roundof(long double number){
-    long double integer=floor(number);
-    long double diff= number-integer;
-    if(diff>0.5){
-        return integer+1.0L;
-    }
-    else if(diff<0.5){
-        return integer;
-    }
-    else{
-        long long x=(long long)integer;
-        if(x%2==0){
-            return integer;
-        }
-        else{
-            return integer+1.0L;
-        }
- 
-    }
-}
-int main(){
-    int n;
-    cin>>n;
-    vector<int>r(n+1);
-    for(int i=1;i<=n;i++){
-        cin>>r[i];
-    }
-    long double expected=0.0L;
-    for( int i=1;i<=n;i++){
-        for( int j=i+1;j<=n;j++){
-            int ra=r[i];
-            int rb=r[j];
-            int m=min(ra,rb);
-            long double res=(long double)(ra*m)-(long double)(m*(m+1)/2);
-            expected += res/(long double)(ra*rb);
- 
+  
+int n, r[105];
+double ans;
+  
+int main() {
+    cin >> n;
+    for (int i = 1; i <= n; i++){
+        cin >> r[i];
+        for (int j = 1; j < i; j++){
+            for (int k = 1; k <= r[i]; k++){
+                if (r[j] > k) ans += (double)(r[j]-k)/r[j]/r[i];
+            }
         }
     }
-    expected+=1e-12L;
-    long double scaled=expected*1e6L;
-    long double ans=roundof(scaled);
-    cout<<fixed<<setprecision(6)<<(ans/1e6L)-1e-12L<<'\n';
-    return 0;
+    cout << fixed << setprecision(6) << ans << "\n";
 }
---
+++ cses192.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        long long nim_sum = 0;
+        for (int i = 0; i < n; i++) {
+            long long x;
+            cin >> x;
+            nim_sum ^= x;
+        }
+
+        if (nim_sum == 0)
+            cout << "second\n";
+        else
+            cout << "first\n";
+    }
+
+    return 0;
+}
